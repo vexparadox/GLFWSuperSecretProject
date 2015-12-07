e0f8bc1029800abd10e6393f56e67d3459d6e26f
William Meaton <will.meaton@gmail.com>
2015-12-07 14:14:24 +0000

    Initial Commit

---
+++ GLFW3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLFW3
+//
+//  Created by William Meaton on 07/12/2015.
+//  Copyright © 2015 WillMeaton.uk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
