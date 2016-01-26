83d1ea8f2eedde5d476b5bf94eadfca6ac0d60e9
William Meaton <will.meaton@gmail.com>
2016-01-26 17:53:12 +0000

    Moved collision to a cpp file to stop header file copying

--- GLFW3/Collision.h
+++ GLFW3/Collision.h
@@ -9,23 +9,13 @@
 #ifndef Collision_h
 #define Collision_h
 #include "Rect.h"
-
 namespace Math {
     //returns if v3 is inside of v1-v2
-    bool isInsideQuad(const Vector2D &v1, const Vector2D &v2, const Vector2D &v3){
-        if(v3.x > v1.x && v3.x < v2.x && v3.y > v1.y && v3.y < v2.y){
-            return true;
-        }
-        return false;
-    }
+    bool isInsideQuad(const Vector2D &v1, const Vector2D &v2, const Vector2D &v3);
     
-    bool isInsideQuad(const Graphics::Rect &r, const Vector2D &v3){
-        return isInsideQuad(r.v, Vector2D(r.w, r.h), v3);
-    }
+    bool isInsideQuad(const Graphics::Rect &r, const Vector2D &v3);
     
-    bool isInsideQuad(float x1, float y1, float x2, float y2, float x3, float y3){
-        return isInsideQuad(Vector2D(x1, x2), Vector2D(x2, y2), Vector2D(x3, y3));
-    }
+    bool isInsideQuad(float x1, float y1, float x2, float y2, float x3, float y3);
 }
 
 #endif /* Collision_h */
